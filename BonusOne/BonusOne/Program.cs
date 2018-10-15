2acfc8061f66e33b283a78185a5529d61fbfefe2
zackeryfinn <zackeryfinn@gmail.com>
2018-10-14 21:50:23 -0400

    The is bonus exercise one

---
+++ BonusOne/BonusOne/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BonusOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //start of loop for go again querry
+
+            while (true)
+            {
+
+                //Greeting and introductions
+               // Console.WriteLine();
+                Console.WriteLine("Welcome to the Letter Grade Converter");
+                Console.Write("Please enter a numerical grade: ");
+
+
+                //Take and validate input for the numerical grade
+
+                float numericalGrade;
+
+                while (!float.TryParse(Console.ReadLine(), result: out numericalGrade) || numericalGrade < 0 || numericalGrade > 100)
+                {
+                    Console.Write("Sorry, that is not a valid input. Please try again: ");
+                }
+
+                Console.WriteLine();
+
+                // Many nested if - else statements to get +/- letter grades
+                if (numericalGrade >= 97)
+                {
+                    Console.WriteLine("Letter grade: A+");
+                }
+                else
+                {
+                    if (numericalGrade >= 93)
+                    {
+                        Console.WriteLine("Letter grade: A");
+                    }
+                    else
+                    {
+                        if (numericalGrade >= 88)
+                        {
+                            Console.WriteLine("Letter grade: A-");
+                        }
+                        else
+                        { if (numericalGrade >= 86)
+                            {
+                                Console.WriteLine("Letter grade: B+");
+                            }
+                        else
+                            { if (numericalGrade >= 83)
+                                {
+                                    Console.WriteLine("Letter grade: B");
+                                }
+                                else
+                                {
+                                    if (numericalGrade >= 80)
+                                    {
+                                        Console.WriteLine("Letter grade: B-");
+                                    }
+                                    else
+                                    {
+                                        if (numericalGrade >= 76)
+                                        {
+                                            Console.WriteLine("Letter grade: C+");
+                                        }
+                                        else
+                                        {
+                                            if (numericalGrade >= 71)
+                                            {
+                                                Console.WriteLine("Letter grade: C");
+                                            }
+                                            else
+                                            {
+                                                if (numericalGrade >= 67)
+                                                {
+                                                    Console.WriteLine("Letter grade: C-");
+                                                }
+                                                else
+                                                {
+                                                    if (numericalGrade >= 66)
+                                                    {
+                                                        Console.WriteLine("Letter grade: D+");
+                                                    }
+                                                    else
+                                                    {
+                                                        if (numericalGrade >= 63)
+                                                        {
+                                                            Console.WriteLine("Letter grade: D");
+                                                        }
+                                                        else
+                                                        {
+                                                            if (numericalGrade >= 61)
+                                                            {
+                                                                Console.WriteLine("Letter grade: D-");
+                                                            }
+                                                            else
+                                                            {
+                                                                Console.WriteLine("Letter grade: F");
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                                
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                            
+                        }
+                    }
+
+                }
+
+                //Section to go again
+               Console.WriteLine();
+
+                Console.Write("Would you like to try again ? If no press \"n\" to exit. -");
+                if (Console.ReadLine().Equals("n", StringComparison.OrdinalIgnoreCase)) break;
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ BonusTwo/BonusTwo/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BonusTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           //being the y/n repeat loop here             
+            while (true)
+            {
+                DateTime birthDate;
+            
+                //Greeting and intro to program
+                Console.WriteLine("Ever wonder how old you are? Enter your birthdate to find out!");
+                Console.Write("Please use the format M/D/YYYY (ex. 10/2/1984): ");
+
+
+                //getting the date input in m/d/yy format
+                string inputBirthDate = Console.ReadLine();
+                while (!DateTime.TryParseExact(inputBirthDate, "M/d/yyyy", null, System.Globalization.DateTimeStyles.None, out birthDate))
+                {
+                    Console.Write("Invalid format, please retry: ");
+                    inputBirthDate = Console.ReadLine();
+                }
+
+
+                //pulling today's date and doing some math
+                DateTime today = DateTime.Today;
+                TimeSpan span = (today - birthDate);
+                int age = (span.Days / 365);
+                Console.WriteLine($"You are {age} years old! Now you know.");
+                Console.WriteLine();
+
+                //Writing a special message if today's yr bday
+                var currentMonth = today.Month;
+                var currentDay = today.Day;
+                var birthMonth = birthDate.Month;
+                var birthDay = birthDate.Day;
+
+                if (currentDay == birthDay && currentMonth == birthMonth)
+                {
+                    Console.WriteLine("More great news: Today is your birthday!");
+                    Console.WriteLine("Just letting you know in case you were unaware.");
+                    Console.WriteLine();
+                }
+
+                
+
+                //time to see if you want to go again
+                Console.Write("Would you like to try again ? If not, please press \"n\" to exit.");
+                if (Console.ReadLine().Equals("n", StringComparison.OrdinalIgnoreCase)) break;
+                Console.WriteLine();
+
+
+
+
+
+
+
+
+
+
+
+            }
+
+
+
+
+        }
+    }
+}
